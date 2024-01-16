d2cb4d25a5134ad02d0bc7fa2fc585b2a14b18b9
daniel <daniel.loedermann@dachdigital.eu>
2024-01-16 12:40:47 +0100

    add new site with jinja

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
-class Polls(models.Model):
-    name = models.Model
+
+class Poll(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return "{0} Slug: {1}".format(self.name, self.slug)
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(to="Poll", on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return "{0} : {1}".format(self.poll.name, self.name)
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Poll, Choice
 
-# Create your views here.
+
+def index(request):
+    context = {"umfragen": Poll.objects.all()}
+    return render(request=request, template_name='polls/index.html', context=context)
