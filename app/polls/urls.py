22addac52eeef8db7a85732d33f4dcd0ee132f44
daniel <daniel.loedermann@dachdigital.eu>
2024-01-17 14:11:19 +0100

    Erstellung der zweiten Seite für Umfragen

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, umfrage_detail
 
 
 urlpatterns = [
     path('', index),
+    path('abstimmung/<str:slug>', umfrage_detail)
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Poll, Choice
 
@@ -6,3 +6,8 @@ from .models import Poll, Choice
 def index(request):
     context = {"umfragen": Poll.objects.all()}
     return render(request=request, template_name='polls/index.html', context=context)
+
+def umfrage_detail(request, slug):
+    umfrage = get_object_or_404(Poll, slug=slug)
+    context = {'umfrage': umfrage}
+    return render(request=request, template_name='polls/umfrage.html', context=context)
