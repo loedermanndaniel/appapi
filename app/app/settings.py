7a0325bbcd1bc411f124990c24857def477e3c9f
daniel <daniel.loedermann@dachdigital.eu>
2024-01-31 14:01:29 +0100

    manage login, not working logout

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls',
     ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,6 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "login"
--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index, PollDetailView, vote, ResultsDetailView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', index, name="index"),
+    path("admin/", admin.site.urls),
+    #path('', TemplateView.as_view(template_name="polls/index.html"), name="index"),
     path('poll/<str:slug>', PollDetailView.as_view(), name="umfrage-detail"),
     path('poll/<str:slug>/vote/', vote, name='vote'),
-    path('poll/<str:slug>/results/', ResultsDetailView.as_view(), name='results')
+    path('poll/<str:slug>/results/', ResultsDetailView.as_view(), name='results'),
+    path('account/', include("django.contrib.auth.urls")),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
